30f66d68a2c61145349afa623726258c1da2ac02
Marco Giering <marco.giering@icloud.com>
2025-07-02 21:26:04 +0200

    new add task and format

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { MainPage } from './main-page/main-page';
 import { ManageTask } from './board/manage-task/manage-task';
 import { Contacts } from './contacts-sektion/contacts/contacts';
+import { AddTask } from './board/add-task/add-task';
 
 export const routes: Routes = [
   { path: '', component: MainPage },
   { path: 'board', component: ManageTask },
   { path: 'contacts', component: Contacts },
+  { path: 'add-task', component: AddTask },
   { path: '**', redirectTo: '' }
 ];

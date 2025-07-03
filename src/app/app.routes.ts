881daae38e43b076a14e66dfccd901e528c68fc9
Markus Fischer <fischer@mideasdesign.de>
2025-07-03 11:01:42 +0200

    add summary component and update the Login / signup page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { MainPage } from './main-page/main-page';
 import { ManageTask } from './board/manage-task/manage-task';
 import { Contacts } from './contacts-sektion/contacts/contacts';
+import { Login } from './login/login';
 
 export const routes: Routes = [
   { path: '', component: MainPage },
   { path: 'board', component: ManageTask },
   { path: 'contacts', component: Contacts },
+    { path: 'login', component: Login },
   { path: '**', redirectTo: '' }
 ];

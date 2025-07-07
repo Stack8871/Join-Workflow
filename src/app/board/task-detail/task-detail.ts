ee75329f5dca0c1de0d292fd8c7f7b7ccce0d3c2
Markus Fischer <fischer@mideasdesign.de>
2025-07-07 09:46:13 +0200

    update the task detail page add the custom checkbox

--- src/app/board/task-detail/task-detail.ts
+++ src/app/board/task-detail/task-detail.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-task-detail',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './task-detail.html',
   styleUrl: './task-detail.scss'
 })
